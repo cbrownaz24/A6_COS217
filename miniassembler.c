199626bd0df9108dde5a8b6bc458f9b9f7d3b23f
Connor Brown <107167121+cbrownaz24@users.noreply.github.com>
2023-12-13 13:12:42 -0500

    added bl instruction

--- miniassembler.c
+++ miniassembler.c
@@ -101,7 +101,27 @@ unsigned int MiniAssembler_b(unsigned long ulAddr,
    uiInstr = 0x14000000;
 
    /* displacement to be split into immlo and immhi and inserted */
-   uiDisp = (unsigned int)((ulAddr - ulAddrOfThisInstr)/4);
+   uiDisp = (unsigned int)((ulAddr - ulAddrOfThisInstr) >> 2);
+
+   setField(uiDisp, 0, &uiInstr, 0, 26);
+
+   return uiInstr;
+}
+
+
+/*--------------------------------------------------------------------*/
+
+unsigned int MiniAssembler_bl(unsigned long ulAddr, 
+   unsigned long ulAddrOfThisInstr)
+{
+   unsigned int uiInstr;
+   unsigned int uiDisp;
+
+   /* Base Instruction Code */
+   uiInstr = 0x94000000;
+
+   /* displacement to be split into immlo and immhi and inserted */
+   uiDisp = (unsigned int)((ulAddr - ulAddrOfThisInstr) >> 2);
 
    setField(uiDisp, 0, &uiInstr, 0, 26);
 
